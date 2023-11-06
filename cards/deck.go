4147bdbd3fbcd31855badd9c54ca6480a913e68c
gehrkev <54125592+gehrkev@users.noreply.github.com>
2023-11-06 17:40:47 -0300

    Section 3 [25-29] - byte slices, write to/read from files

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -26,4 +30,26 @@ func newDeck() deck {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck()
+
+		// Option #2 - log the error and entirely quit the program
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := newDeck()
-	//cards.print()
-	hand, remainingCards := deal(cards, 5)
-
-	fmt.Println("Hand")
-	hand.print()
-	fmt.Println("Deck")
-	remainingCards.print()
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
